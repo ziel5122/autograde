5ae1dfaf2a192270b6133ab2553bdc7cb1c23e52
ziel5122 <azielinski@csumb.edu>
2017-02-14 01:29:05 -0800

    backend calls a wrapper script that keeps track of output, error, and runtime. Script is passed actual script as argument and file. Need to add support of file type suffixes. Merging into master

--- API/DockerSandboxMin.js
+++ API/DockerSandboxMin.js
@@ -38,9 +38,10 @@ var DockerSandboxMin = function(vm_name, wdir, folder, hw_num, hw_type, content,
 */
 
 DockerSandboxMin.prototype.run = function(success) {
+    var sandbox = this;
     
     this.prepare(function() {
-        //this.execute(success);
+        sandbox.execute(success);
     });
 }
 
@@ -66,10 +67,10 @@ DockerSandboxMin.prototype.prepare = function(success) {
 			console.log('* mkdir ' + this.wdir + this.folder);
 
 			//copy scripts to new folder
-			exec('cp ' + this.wdir + '/Payload/* ' + this.wdir + this.folder, (err) => {
+			exec('cp ' + this.wdir + '../solutions/hw' + this.hw_num + '/' + this.hw_type + '.sh ' + this.wdir + this.folder + ' && cp ' + this.wdir + 'Payload/wrapper.sh ' + this.wdir + this.folder, (err) => {
 				if (err) console.error('error copying payload to new folder');
 				else {
-					console.log('* cp ' + this.wdir + '/Payload/* ' + this.wdir + this.folder);
+					console.log('* cp ' + this.wdir + '../solutions/hw' + this.hw_num + '/' + this.hw_type + '.sh ' + this.wdir + this.folder + ' && cp ' + this.wdir + 'Payload/wrapper.sh ' + this.wdir + this.folder);
 
 					//give all permission on new folder
 					exec('chmod 777 ' + this.wdir + this.folder, (err) => {
@@ -78,7 +79,7 @@ DockerSandboxMin.prototype.prepare = function(success) {
 							console.log('* chmod 777 ' + this.wdir + this.folder);
 
 							//write content to file
-							fs.writeFile(this.wdir + this.folder + '/file', this.content, (err) => {
+							fs.writeFile(this.wdir + this.folder + '/file.c', this.content, (err) => {
 								if (err) console.error('error writing user content file');
 								else {
 									console.log('* user content file saved');
@@ -109,16 +110,14 @@ DockerSandboxMin.prototype.prepare = function(success) {
          * @param {Function pointer} success ?????
 */
 
-DockerSandboxMin.prototype.execute = function() {
-    var exec = require('child_process').exec;
-    var fs = require('fs');
+DockerSandboxMin.prototype.execute = function(success) {
     var myC = 0; //variable for timeout
+    var sandbox = this;
 
     //statement to be executed
-    var st = this.path + 'DockerTimeout.sh ' + this.timeout +
-        's -e \'NODE_PATH=/usr/local/lib/node_modules\' -i -t -v "' + this.path +
-        this.folder + '":/usercode ' + this.vm_name + ' /usercode/script.sh ' +
-        this.compiler + ' ' + this.fname + ' args ' + this.xargs;
+    var st = this.wdir + 'DockerTimeout.sh ' + this.timeout +
+        's -e \'NODE_PATH=/usr/local/lib/node_modules\' -i -t -v "' + this.wdir +
+        this.folder + '":/usercode ' + this.vm_name + ' /usercode/wrapper.sh /usercode/' + this.hw_type + '.sh /usercode/file.c'
 
     //print satement to console
     console.log(st);
@@ -130,28 +129,29 @@ DockerSandboxMin.prototype.execute = function() {
     //Check For File named "completed" after every 1 second
     var intid = setInterval(function() {
             //Displaying the checking message after 1 second interval, testing purposes only
-            console.log("Checking " + this.path+this.folder + ": for completion: " + myC);
+            console.log("Checking " + sandbox.wdir + sandbox.folder + ": for completion: " + myC);
             myC = myC + 1;
 
-            fs.readFile(this.path + this.folder + '/completed', 'utf8', function(err, data) {
+            fs.readFile(sandbox.wdir + sandbox.folder + '/completed', 'utf8', function(err, data) {
                 //if file is not available yet and the file interval is not yet up carry on
-                if (err && myC < this.timeout_value) {
+                if (err && myC < sandbox.timeout) {
                     //console.log(err);
                     return;
                 }
                 //if file is found simply display a message and proceed
-                else if (myC < this.timeout_value) {
+                else if (myC < sandbox.timeout) {
                     console.log("DONE");
                     //check for possible errors
-                    fs.readFile(this.path + this.folder + '/errors', 'utf8', function(err2, data2) {
+                    fs.readFile(sandbox.wdir + sandbox.folder + '/errors', 'utf8', function(err2, data2) {
                     	if (!data2) data2 = "";
                    		console.log("Error file: ");
                    		console.log(data2);
 
-                   		console.log("Main File");
+                   		console.log("Main File:");
                    		console.log(data);
 
-            			var lines = data.toString().split('*-COMPILEBOX::ENDOFOUTPUT-*');
+            			//var lines = data.toString().split('*-COMPILEBOX::ENDOFOUTPUT-*');
+            			var lines = data.toString().split('EXECUTION COMPLETE');
             			data = lines[0];
             			var time = lines[1];
 
@@ -166,11 +166,11 @@ DockerSandboxMin.prototype.execute = function() {
                 //if time is up. Save an error message to the data variable
                 else {
                 	//Since the time is up, we take the partial output and return it.
-                	fs.readFile(this.path + this.folder + '/logfile.txt', 'utf8', function(err, data) {
+                	fs.readFile(sandbox.wdir + sandbox.folder + '/logfile.txt', 'utf8', function(err, data) {
                 		if (!data) data = "";
                         data += "\nExecution Timed Out";
-                        console.log("Timed Out: " + this.folder + " " + this.langName);
-                        fs.readFile(this.path + this.folder + '/errors', 'utf8', function(err2, data2) {
+                        console.log("Timed Out: " + sandbox.folder);
+                        fs.readFile(sandbox.wdir + sandbox.folder + '/errors', 'utf8', function(err2, data2) {
     	                	if(!data2) data2 = "";
 
             				var lines = data.toString().split('*---*');
@@ -186,9 +186,9 @@ DockerSandboxMin.prototype.execute = function() {
                 }
 
             //now remove the temporary directory
-            console.log("ATTEMPTING TO REMOVE: " + this.folder);
+            console.log("ATTEMPTING TO REMOVE: " + sandbox.folder);
             console.log("------------------------------")
-            exec("rm -r " + this.folder);
+            exec("rm -r " + sandbox.folder);
 
             clearInterval(intid);
         });
