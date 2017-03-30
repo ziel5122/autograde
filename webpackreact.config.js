0d883b8eba2201e7db3ee4217a1d409c32c99018
ziel5122 <azielinski@csumb.edu>
2017-03-30 01:18:22 -0700

    fused reloadable example with core repo

---
+++ webpackreact.config.js
@@ -0,0 +1,144 @@
+const autoprefixer = require('autoprefixer');
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
+
+const nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(x => {
+    return ['.bin'].indexOf(x) === -1;
+  }).forEach(mod => {
+    nodeModules[mod] = `commonjs ${mod}`;
+  });
+
+const common = {
+  module: {
+    // First, run the linter.
+    // It's important to do this before Babel processes the JS.
+    preLoaders: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: 'eslint',
+        include: path.join(__dirname, 'src')
+      }
+    ],
+    loaders: [
+      // ** ADDING/UPDATING LOADERS **
+      // The "url" loader handles all assets unless explicitly excluded.
+      // The `exclude` list *must* be updated with every change to loader extensions.
+      // When adding a new loader, you must add its `test`
+      // as a new entry in the `exclude` list for "url" loader.
+
+      // "url" loader embeds assets smaller than specified size as data URLs to avoid requests.
+      // Otherwise, it acts like the "file" loader.
+      {
+        exclude: [
+          /\.html$/,
+          // We have to write /\.(js|jsx)(\?.*)?$/ rather than just /\.(js|jsx)$/
+          // because you might change the hot reloading server from the custom one
+          // to Webpack's built-in webpack-dev-server/client?/, which would not
+          // get properly excluded by /\.(js|jsx)$/ because of the query string.
+          // Webpack 2 fixes this, but for now we include this hack.
+          // https://github.com/facebookincubator/create-react-app/issues/1713
+          /\.(js|jsx)(\?.*)?$/,
+          /\.css$/,
+          /\.json$/,
+          /\.svg$/
+        ],
+        loader: 'url',
+        query: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      },
+      // Process JS with Babel.
+      {
+        test: /\.(js|jsx)$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel',
+        query: {
+          // This is a feature of `babel-loader` for webpack (not Babel itself).
+          // It enables caching results in ./node_modules/.cache/babel-loader/
+          // directory for faster rebuilds.
+          cacheDirectory: true
+        }
+      },
+      // "postcss" loader applies autoprefixer to our CSS.
+      // "css" loader resolves paths in CSS and adds assets as dependencies.
+      // "style" loader turns CSS into JS modules that inject <style> tags.
+      // In production, we use a plugin to extract that CSS to a file, but
+      // in development "style" loader enables hot editing of CSS.
+      {
+        test: /\.css$/,
+        loader: 'style!css?importLoaders=1!postcss'
+      },
+      // JSON is not enabled by default in Webpack but both Node and Browserify
+      // allow it implicitly so we also enable it.
+      {
+        test: /\.json$/,
+        loader: 'json'
+      },
+      // "file" loader for svg
+      {
+        test: /\.svg$/,
+        loader: 'file',
+        query: {
+          name: 'static/media/[name].[hash:8].[ext]'
+        }
+      }
+      // ** STOP ** Are you adding a new loader?
+      // Remember to add the new extension(s) to the "url" loader exclusion list.
+    ]
+  },
+
+  // We use PostCSS for autoprefixing only.
+  postcss: function() {
+    return [
+      autoprefixer({
+        browsers: [
+          '>1%',
+          'last 4 versions',
+          'Firefox ESR',
+          'not ie < 9', // React doesn't support IE8 anyway
+        ]
+      }),
+    ];
+  }
+};
+
+const frontend = {
+  name: 'frontend code; output to ./public',
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-hot-middleware/client',
+    path.resolve(__dirname, 'src/index.jsx')
+  ],
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'frontend-bundle.js',
+    publicPath: '/'
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
+
+const backend = {
+  name: 'server code; output to ./public',
+  entry: path.resolve(__dirname, 'src/server/app.js'),
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'server-bundle.js',
+    publicPath: '/'
+  },
+  target: 'node',
+  externals: nodeModules,
+  plugins: [
+    new webpack.IgnorePlugin(/\.(css|less)$/)
+  ]
+};
+
+module.exports = [
+  Object.assign({}, common, frontend),
+  Object.assign({}, common, backend)
+]
