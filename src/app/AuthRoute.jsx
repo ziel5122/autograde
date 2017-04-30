980aa31244b3e48e9b829df7a931032503583e9e
ziel5122 <azielinski@csumb.edu>
2017-04-30 02:11:38 -0700

    login and logout work with credentials. working on demo next then assignment logic

--- src/app/AuthRoute.jsx
+++ src/app/AuthRoute.jsx
@@ -3,27 +3,29 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route } from 'react-router';
 
-const AuthRoute = ({ isAuthenticated, component: Component, ...rest }) => (
-  <Route
-    render={props => (
-      isAuthenticated ?
-        <Component {...props} /> :
-        <Redirect
-          to={{
-            pathname: '/login',
-            state: {
-              from: props.location,
-            },
-          }}
-        />
-    )}
-    {...rest}
-  />
-);
+const AuthRoute = ({ isAuthenticated, component: Component, ...rest }) => {
+  console.log('authroute render');
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        isAuthenticated ?
+          <Component {...props} /> :
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: {
+                from: props.location,
+              },
+            }}
+          />
+      )}
+    />
+  );
+};
 
 const AuthRouteRedux = connect(
   state => ({ isAuthenticated: state.isAuthenticated }),
-  null,
 )(AuthRoute);
 
 export default AuthRouteRedux;
--- src/app/Main.jsx
+++ src/app/Main.jsx
@@ -1,26 +1,17 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import React from 'react';
-import { Provider } from 'react-redux';
 
 import Layout from './Layout';
-import configureStore from '../data/configureStore';
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
 
-const store = configureStore();
-
-const Main = () => {
-  console.log(store.getState());
-  return (
-    <MuiThemeProvider>
-      <Provider store={store}>
-        <Layout />
-      </Provider>
-    </MuiThemeProvider>
-  );
-};
+const Main = () => (
+  <MuiThemeProvider>
+    <Layout />
+  </MuiThemeProvider>
+);
 
 export default Main;
