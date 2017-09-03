1d3e9625dba9056306e4ce32304f1c9afca3b1ec
ziel5122 <azielinski@csumb.edu>
2017-09-03 04:45:23 -0700

    Fixed request promise chain so Id is passed correctly. Changed 'runCode' function into a promise. Added final step to clean up after response is sent

--- code/hw2/script.sh
+++ code/hw2/script.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 cd /code
-gcc student_src.c -o student_exe 2>&1
-ls
+gcc student_src.c -o student_exe
 ./student_exe
--- code/script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /code
-gcc test.c -o test
-./test
