e8ee02ddddbac9f8528c6440be1942bc0e7fd087
ziel5122 <azielinski@csumb.edu>
2017-09-10 02:49:37 -0700

    updated deployment to include everything in one script

--- scripts/build.js
+++ scripts/build.js
@@ -5,16 +5,25 @@ const webpack = require('webpack');
 const clientConfig = require('../config/webpack.client.prod');
 const copySync = fs.copySync;
 
-const API_DIR = join(__dirname, '../src/api');
 const CODE_DIR = join(__dirname, '../code');
 const DIST_DIR = join(__dirname, '../dist');
-const DOCKER_DIR = join(__dirname, '../src/docker');
 const ENV_PATH = join(__dirname, '../.env');
-const GCP_DIR = join(__dirname, '../src/gcp');
 const INDEX_PATH = join(__dirname, '../public/index.html');
-const PACKAGE_JSON_PATH = join(__dirname, '../public/package.json');
+
+const DOCKERFILE_PATH = join(__dirname, '../config/Dockerfile');
+const PACKAGE_JSON_PATH = join(__dirname, '../config/package.json');
+const YAML_PATH = join(__dirname, '../config/app.yaml');
+const DS_CREDS_PATH = join(__dirname, '../config/datastore-credentials.json');
+
+const API_DIR = join(__dirname, '../src/api');
+const DOCKER_DIR = join(__dirname, '../src/docker');
+const GCP_DIR = join(__dirname, '../src/gcp');
 const SERVER_PATH = join(__dirname, '../src/server.js');
 const UTILS_DIR = join(__dirname, '../src/utils');
+
+const DEPLOY_PATH = join(__dirname, '../scripts/deploy.sh');
+const UPDATE_DOCKER_PATH = join(__dirname, '../scripts/update_docker.sh');
+const UPDATE_HW_PATH = join(__dirname, '../scripts/update_hw.sh');
 
 fs.removeSync(DIST_DIR);
 
@@ -31,4 +40,9 @@ copySync(INDEX_PATH, join(DIST_DIR, 'src/index.html'));
 copySync(PACKAGE_JSON_PATH, join(DIST_DIR, 'package.json'))
 copySync(SERVER_PATH, join(DIST_DIR, 'src/server.js'));
 copySync(UTILS_DIR, join(DIST_DIR, 'src/utils'));
-copySync(join(__dirname, '../config/app.yml'), join(DIST_DIR, 'app.yaml'));
+copySync(YAML_PATH, join(DIST_DIR, 'app.yaml'));
+copySync(DEPLOY_PATH, join(DIST_DIR, 'scripts/deploy.sh'));
+copySync(DOCKERFILE_PATH, join(DIST_DIR, 'config/Dockerfile'));
+copySync(UPDATE_DOCKER_PATH, join(DIST_DIR, 'scripts/update_docker.sh'));
+copySync(UPDATE_HW_PATH, join(DIST_DIR, 'scripts/update_hw.sh'));
+copySync(DS_CREDS_PATH, join(DIST_DIR, 'config/datastore-credentials.json'));
