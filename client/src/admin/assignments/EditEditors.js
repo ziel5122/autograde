ec9defe8caee042881f4f7e8d0c9108db0c7a6eb
ziel5122 <azielinski@csumb.edu>
2017-10-19 00:21:10 -0700

    small changes to editing

--- client/src/admin/assignments/EditEditors.js
+++ client/src/admin/assignments/EditEditors.js
@@ -1,34 +1,120 @@
-import ContentAdd from 'material-ui/svg-icons/content/add';
-import React from 'react';
+import Add from 'material-ui/svg-icons/content/add';
+import Remove from 'material-ui/svg-icons/content/remove';
+import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { v4 } from 'uuid';
 
 import EditorForm from './EditorForm';
 
-const EditEditors = ({ editors, match: { params: { partId } }, parts }) => {
-  const { editorIds } = parts[partId];
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <td>Filename</td>
-          <td>Type</td>
-          <td>Title</td>
-        </tr>
-      </thead>
-      <tbody>{
-        editorIds.map(editorId => (
-          <EditorForm editor={editors[editorId]} key={editorId} />
-        ))
-      }</tbody>
-      <tfoot><tr><td><ContentAdd /></td></tr></tfoot>
-    </table>
-  );
+const buttonStyle = {
+  /* eslint-disable no-dupe-keys */
+  cursor: 'hand',
+  cursor: 'pointer',
+  /* eslint-enable no-dupe-keys */
 };
+
+class EditEditors extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    const { editors, match: { params: { partId } }, parts } = this.props;
+    const { editorIds } = parts[partId];
+
+    this.state = {
+      editorIds,
+      editors: editorIds.reduce((editorsMap, editorId) => {
+        editorsMap[editorId] = editors[editorId];
+        return editorsMap;
+      }, {}),
+    };
+
+    this.addEditor = this.addEditor.bind(this);
+    this.removeEditor = this.removeEditor.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    const { editors, match: { params: { partId } }, parts } = nextProps;
+    const { editorIds } = parts[partId];
+
+    this.setState({
+      editorIds,
+      editors: editorIds.reduce((editorsMap, editorId) => {
+        editorsMap[editorId] = editors[editorId];
+        return editorsMap;
+      }, {}),
+    });
+  }
+
+  addEditor() {
+    const { editorIds, editors } = this.state;
+    const editorId = v4();
+    this.setState({
+      editorIds: [...editorIds, editorId],
+      editors: {
+        ...editors,
+        [editorId]: {
+          filename: '',
+          title: false,
+          type: '',
+        },
+      }
+    });
+  }
+
+  removeEditor() {
+    const { editorIds, editors } = this.state;
+    const lastIndex = editorIds.length - 1;
+    const editorId = editorIds[lastIndex];
+    const { [editorId]: {}, ...rest } = editors;
+    this.setState({
+      editorIds: editorIds.slice(0, lastIndex),
+      editors: rest,
+    });
+  }
+
+  render() {
+    console.log(this.state);
+    const { editorIds, editors } = this.state;
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td>Filename</td>
+            <td>Type</td>
+            <td>Title</td>
+          </tr>
+        </thead>
+        <tbody>{
+          editorIds.map(editorId => {
+            const editor = editors[editorId];
+            return (
+              <EditorForm
+                addEditor={this.addEditor}
+                editor={editor}
+                key={editorId}
+                id={editorId}
+              />
+            );
+          })
+        }</tbody>
+        <tfoot>
+          <tr>
+            <td>
+              <Add onClick={this.addEditor} style={buttonStyle} />
+              <Remove onClick={this.removeEditor} style={buttonStyle} />
+            </td>
+          </tr>
+        </tfoot>
+      </table>
+    );
+  }
+}
 
 const mapStateToProps = ({ editors, parts }) => ({
   editors,
   parts,
 });
 
-export default connect(mapStateToProps)(EditEditors);
+export default withRouter(connect(mapStateToProps)(EditEditors));
--- client/src/admin/assignments/EditorForm.js
+++ client/src/admin/assignments/EditorForm.js
@@ -1,13 +1,13 @@
 import Done from 'material-ui/svg-icons/action/done';
 import React from 'react';
 
-const EditorForm = ({ editor: { filename, title, type }, key }) => (
+const EditorForm = ({ addEditor, editor: { filename, title, type }, id }) => (
   <tr>
     <td>
-      <input defaultValue={filename} id={`${key}filename`} />
+      <input defaultValue={filename} id={`${id}filename`} />
     </td>
     <td>
-      <select defaultValue={type} id={`${key}select`}>
+      <select defaultValue={type} id={`${id}select`}>
         <option value="C">C</option>
         <option value="bash">bash</option>
       </select>
