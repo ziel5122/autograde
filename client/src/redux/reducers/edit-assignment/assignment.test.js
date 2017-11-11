1fb881abf42d5c5d11ec4155a64ecf868a4c98af
Austin Zielinski <azielinski@csumb.edu>
2017-11-10 22:28:03 -0800

    wrote 3 tests for editAssignment->assignment reducer

---
+++ client/src/redux/reducers/edit-assignment/assignment.test.js
@@ -0,0 +1,105 @@
+import { createStore } from 'redux';
+
+import { addPartId, clearAssignment, removePartId } from '../../actions/edit-assignment/assignment';
+import reducers from '..';
+
+const emptyState = {
+  auth: {
+    admin: false,
+    errorText: '',
+    loggedIn: false,
+    password: '',
+    username: '',
+  },
+  data: {
+    assignmentIds: [],
+    assignments: {},
+    editors: {},
+    parts: {},
+  },
+  editAssignment: {
+    assignment: {},
+    editors: {},
+    parts: {},
+  },
+  user: {
+    parts: {},
+  },
+};
+
+describe('tests for the editAssignment->assignment reducer', () => {
+  it('should add a part id to the end of the partIds array', () => {
+    const initialState = {
+      editAssignment: {
+        assignment: {
+          partIds: ['1234567890'],
+        },
+      },
+    };
+    const store = createStore(reducers, initialState);
+    const partId = '0987654321';
+    const finalState = {
+      ...emptyState,
+      editAssignment: {
+        ...emptyState.editAssignment,
+        assignment: {
+          partIds: [
+            '1234567890',
+            '0987654321',
+          ],
+        },
+      },
+    };
+
+    store.dispatch(addPartId(partId));
+    const state = store.getState();
+
+    expect(state).toEqual(finalState);
+  });
+
+  it('should set the assignment state to an empty object', () => {
+    const initialState = {
+      editAssignment: {
+        assignment: {
+          dueDate: '2017-08-08',
+          name: 'assignment 1',
+          partIds: ['1234567890'],
+          visible: true,
+        },
+      },
+    };
+    const store = createStore(reducers, initialState);
+
+    store.dispatch(clearAssignment());
+    const state = store.getState();
+
+    expect(state).toEqual(emptyState);
+  });
+
+  it('should remove a part id from the end of the partIds array', () => {
+    const initialState = {
+      editAssignment: {
+        assignment: {
+          partIds: ['1234567890', '0987654321'],
+        },
+      },
+    };
+    const store = createStore(reducers, initialState);
+    const finalState = {
+      ...emptyState,
+      editAssignment: {
+        ...emptyState.editAssignment,
+        assignment: {
+          partIds: [
+            '1234567890',
+          ],
+        },
+      },
+    };
+
+    store.dispatch(removePartId());
+    const state = store.getState();
+
+    expect(state).toEqual(finalState);
+  });
+});
